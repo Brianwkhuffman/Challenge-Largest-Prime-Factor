ffec9408727b8e56e35a73457dd92759065af771
Briii <brianwkhuffman@gmail.com>
2019-07-13 10:45:32 -1000

    Finished

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,15 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-  // do your work here
+  let numArr = [];
+
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      numArr.push(i);
+      n /= i;
+    }
+  }
+  let highNum = numArr[numArr.length - 1];
+  primeNumber = highNum;
 
   return primeNumber;
 };
